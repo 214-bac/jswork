42489bd4f7f214249ba212457e3efc71e7565cbf
Ataal <2568232059@qq.com>
2019-10-21 18:24:46 +0800

    work7

--- work7 slide/utils.js
+++ work7 slide/utils.js
@@ -33,11 +33,11 @@ function showLog(logValues, showElement){
 function insertSort(str) {
     let arr = str.split(',')
     let sortLog = []
-    for (let i = 1;i < arr.length;i++){
+    for (let i = 1;i < arr.length ;i++){
         for (let j = i;j > 0;j--){
-            sortLog.push([arr.concat(),[j,j + 1]])
-            sortLog.push([arr.concat(),[j,j + 1]])
-            sortLog.push([arr.concat(),[j,j + 1]])
+            sortLog.push([arr.concat(),[j,j - 1]])
+            sortLog.push([arr.concat(),[j,j - 1]])
+            sortLog.push([arr.concat(),[j,j - 1]])
             if (arr[j - 1] > arr[j]) {
                 [arr[j - 1],arr[j]] = [arr[j],arr[j - 1]]
                 sortLog.push([arr.concat(),[j,j + 1]])
