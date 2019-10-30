047078296e3a7c45269fec27daf65dad73f1894f
Ataal <2568232059@qq.com>
2019-10-30 19:48:53 +0800

    work10

--- work10/index.js
+++ work10/index.js
@@ -21,3 +21,10 @@ function monkey(){
     }
     document.getElementById('monkeyking').innerText = monkey[0]
 }
+function stat(){
+    let str = document.getElementById("str").value
+    let obj = {}
+    str.split('').reduce((ata,al) => {
+        ata[al]?ata[al]++:ata[al]=1;return obj},{})
+    document.getElementById('result').innerText = JSON.stringify(obj)
+}
